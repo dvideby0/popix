17db984dd250958b41e2b414595bd58d12c2d8d3
dvideby0 <rbrookfield@yahoo.com>
2012-06-09 03:45:22 -0400

    photo sending working

--- popix/www/lib/js/main.js
+++ popix/www/lib/js/main.js
@@ -1,13 +1,16 @@
+var socket = io.connect('http://apps.moby.io:8989');
 function TakePicture(){
-    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI });
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 20,
+        destinationType: Camera.DestinationType.DATA_URL });
 
-    function onSuccess(imageURI) {
-        var image = document.getElementById('myImage');
-        image.src = imageURI;
+    function onSuccess(imageData) {
+        socket.emit('ClientSendImage', imageData);
     }
 
     function onFail(message) {
         alert('Failed because: ' + message);
     }
 }
+socket.on('NewImage', function(msg){
+    $('#myImage').attr('src', 'data:image/jpeg;base64,' + msg);
+});
