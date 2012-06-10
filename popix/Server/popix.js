485aa3127c2bd38f28cec6a1a5e70afba82885e2
dvideby0 <rbrookfield@yahoo.com>
2012-06-10 08:40:21 -0400

    New Server App

--- popix/Server/popix.js
+++ popix/Server/popix.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var Mongolian = require("mongolian");
+var server = new Mongolian;
 var app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end();
@@ -6,12 +8,21 @@ var app = http.createServer(function(req, res) {
 var Users = 0;
 var io = require('socket.io').listen(app);
 io.set('log level', 1);
-io.set('transports', ['websocket']);
+var db = server.db("popix");
+var posts = db.collection("posts");
 io.sockets.on('connection', function(socket) {
     Users = Users + 1;
     io.sockets.emit('UserCount', Users);
     socket.on('ClientSendImage', function(msg){
-        socket.broadcast.emit('NewImage', msg);
+        socket.broadcast.emit('NewImage', {
+            Image: msg,
+            Sender: socket.id
+        });
+        posts.insert({
+            User: socket.id,
+            Created: new Date,
+            Image: msg
+        });
     });
     socket.on('disconnect', function () {
         Users = Users - 1;
